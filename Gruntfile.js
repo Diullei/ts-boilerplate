2f061025547f664e24ff81f4cfe16aab493ac6fd
Diullei Gomes <diullei@gmail.com>
2014-05-18 21:09:41 -0300

    added clean task to grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,46 +1,36 @@
 module.exports = function(grunt) {
     'use strict';
 
-    // https://github.com/gruntjs/grunt-contrib-jasmine
-    grunt.loadNpmTasks('grunt-contrib-jasmine');
-    // https://github.com/grunt-ts/grunt-ts
-    grunt.loadNpmTasks('grunt-ts');
-    // https://github.com/gruntjs/grunt-contrib-jshint
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    // https://github.com/palantir/grunt-tslint
-    grunt.loadNpmTasks('grunt-tslint');
+    grunt.loadNpmTasks('grunt-contrib-jasmine');    // https://github.com/gruntjs/grunt-contrib-jasmine
+    grunt.loadNpmTasks('grunt-ts');                 // https://github.com/grunt-ts/grunt-ts
+    grunt.loadNpmTasks('grunt-contrib-clean');      // https://github.com/gruntjs/grunt-contrib-clean
+    grunt.loadNpmTasks('grunt-contrib-jshint');     // https://github.com/gruntjs/grunt-contrib-jshint
+    grunt.loadNpmTasks('grunt-tslint');             // https://github.com/palantir/grunt-tslint
 
     grunt.initConfig({
+        clean: ['bin/', 'test/reports'],            // Wipe out previous builds and test reporting.
+
         ts: {
             build: {
-                // The source TypeScript files, http://gruntjs.com/configuring-tasks#files
-                src: ['src/**/*.ts'],
-                // If specified, generate this file that to can use for reference management
-                reference: './src/_reference.ts',
-                // If specified, the generate JavaScript files are placed here. Only works if out is not specified
-                outDir: 'bin',
-                // Use to override the default options, http://gruntjs.com/configuring-tasks#options
-                options: {
-                    // 'es3' (default) | 'es5'
-                    target: 'es3',
-                    // 'amd' (default) | 'commonjs'    
-                    module: 'commonjs',
-                    // true (default) | false
-                    sourceMap: false,
-                    // true | false (default)
-                    declaration: false,
-                    // true (default) | false
-                    removeComments: true
+                src: ['src/**/*.ts'],               // The source TypeScript files, http://gruntjs.com/configuring-tasks#files
+                reference: './src/_reference.ts',   // If specified, generate this file that to can use for reference management
+                outDir: 'bin',                      // If specified, the generate JavaScript files are placed here. Only works if out is not specified
+                options: {                          // Use to override the default options, http://gruntjs.com/configuring-tasks#options
+                    target: 'es3',                  // 'es3' (default) | 'es5'
+                    module: 'commonjs',             // 'amd' (default) | 'commonjs'    
+                    sourceMap: false,               // true (default) | false
+                    declaration: false,             // true | false (default)
+                    removeComments: true            // true (default) | false
                 },
             },
-            test: {
-                // The source TypeScript files, http://gruntjs.com/configuring-tasks#files
+            test: {                                 // The source TypeScript files, http://gruntjs.com/configuring-tasks#files
                 src: ['test/**/*.ts'],
                 options: {
                     sourceMap: true,
                 },
             }
         },
+
         jasmine: {
             pivotal: {
                 src: 'src/**/*.js',
@@ -50,12 +40,14 @@ module.exports = function(grunt) {
                 }
             }
         },
+
         jshint: {
             all: ['Gruntfile.js'],
             options: {
                 jshintrc: '.jshintrc'
             }
         },
+
         tslint: {
             options: {
                 configuration: grunt.file.readJSON('tslintrc.json')
@@ -66,8 +58,8 @@ module.exports = function(grunt) {
         }
     });
 
-    grunt.registerTask('check-code-style', ['jshint:all', 'tslint:all']);
-    grunt.registerTask('build', ['ts:build', 'ts:test']);
-    grunt.registerTask('test', ['check-code-style', 'build', 'jasmine:pivotal']);
-    grunt.registerTask('default', ['test']);
+    grunt.registerTask('check-code-style',  ['jshint:all', 'tslint:all']);
+    grunt.registerTask('build',             ['ts:build', 'ts:test']);
+    grunt.registerTask('test',              ['clean', 'check-code-style', 'build', 'jasmine:pivotal']);
+    grunt.registerTask('default',           ['test']);
 };
